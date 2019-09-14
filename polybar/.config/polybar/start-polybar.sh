22755e2b7ecd165b605d1682404638034b6a0ac1
Shaun Reed <shaunrd0@gmail.com>
2019-09-14 06:08:28 -0400

    Update Polybar start script

--- polybar/.config/polybar/start-polybar.sh
+++ polybar/.config/polybar/start-polybar.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+## Author: Shaun Reed | Contact: shaunrd0@gmail.com | URL: www.shaunreed.com ##
+##  A script placed in ~/.config/polybar/ - Uses ${env:MONITOR}              ##
+##  Starts polybars top and bottom on multiple displays                      ##
+###############################################################################
+# start-polybar.sh
+
+# Kill any previous polybars
+pkill -f polybar
+
+# For each monitor in list up to ':'
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload top &
-    MONITOR=$m polybar --reload bottom &
+  # Reload polybars with monitor device name
+  MONITOR=$m polybar --reload top &
+  MONITOR=$m polybar --reload bottom &
 done
-#for m in $(polybar --list-monitors | cut -d":" -f1); do
-#    MONITOR=$m polybar --reload bottom &
-#done
 
