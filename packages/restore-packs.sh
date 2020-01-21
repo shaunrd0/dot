14838aee94b25e0599c5f9e5503ae99832fd5263
Shaun Reed <shaunrd0@gmail.com>
2020-01-21 03:56:53 +0000

    Update restore of packages

--- packages/restore-packs.sh
+++ packages/restore-packs.sh
@@ -7,5 +7,8 @@ sudo cp -R sources.list* /etc/apt/
 sudo apt-get update
 sudo apt-get install dselect
 sudo dpkg --set-selections < package.list
-sudo dselect
+sudo apt-get dselect-upgrade
+sudo pip3 install powerline-shell
+go get -u github.com/arl/gitmux
+
 
