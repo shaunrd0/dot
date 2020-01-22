282dd12615f4eacafba26ed1825800470777828e
Shaun Reed <shaunrd0@gmail.com>
2020-01-22 16:41:32 +0000

    Update i3-debian packages

--- packages/restore-packs.sh
+++ packages/restore-packs.sh
@@ -8,6 +8,9 @@ sudo apt-get update
 sudo apt-get install dselect
 sudo dpkg --set-selections < package.list
 sudo apt-get dselect-upgrade
+# Create symlink for libclang
+# Required for C++ clang_completion vim plugin
+sudo ln -s /usr/lib/llvm-8/lib/libclang.so.1 /usr/lib/llvm-8/lib/libclang.so
 sudo pip3 install powerline-shell
 go get -u github.com/arl/gitmux
 
