7fa1cfe21fc09a3b18a68a7bae3daf9fa9a07b80
Shaun Reed <shaunrd0@gmail.com>
2020-03-24 10:54:45 -0400

    Add battery module to polybar, update start-polybar.sh to kill previously running bars

--- .config/polybar/start-polybar.sh
+++ .config/polybar/start-polybar.sh
@@ -6,6 +6,11 @@
 # start-polybar.sh
 
 # Kill any previous polybars
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 
 # For each monitor in list up to ':'
 for m in $(polybar --list-monitors | cut -d":" -f1); do
